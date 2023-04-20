4c2459df3b82fafbf794010288dabfb1582f5791
harsh500 <harsh.trivedi.mikki@gmail.com>
2023-04-20 17:01:49 +0530

    nothing

--- src/test_simple.java
+++ src/test_simple.java
@@ -1,9 +1,14 @@
-class MyClass{
-    void foob()
+public class MyClass {
+    
+
+    // function to check if a given string is a palindrome
+    public static void show(int a) 
     {
+        System.out.println(a);
     }
-    public static void main()
-    {
-        int a=5;
+
+    public static void main() {
+        // remove whitespace and convert to lowercase
+        show(5);
     }
 }
