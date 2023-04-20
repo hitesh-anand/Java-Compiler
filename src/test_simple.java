1a97234b512c4fe66b085181ef3f8860c6f76251
hitesh-anand <anandhitesh03@gmail.com>
2023-04-20 19:21:16 +0530

    Fixed printing error

--- src/test_simple.java
+++ src/test_simple.java
@@ -4,7 +4,10 @@ public class MyClass {
     // function to check if a given string is a palindrome
     public void show(int a) 
     {
-        System.out.println(a);
+        // System.out.println(a);
+        a = 2;
+        if(a==1)
+            a++;
     }
 
     public static void main() {
